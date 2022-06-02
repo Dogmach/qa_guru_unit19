13e25f0e0193c76e70e2997bf4428a20f6bc4ec6
Nikita Sukhodoev <dogmach94@yandex.ru>
2022-06-02 22:45:21 +0400

    add new files

--- src/test/java/tests/BookstoreTests.java
+++ src/test/java/tests/BookstoreTests.java
@@ -1,4 +1,48 @@
 package tests;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+
 public class BookstoreTests {
+    @BeforeAll
+    static void beforeAll() {
+        RestAssured.baseURI = "https://demoqa.com/";
+    }
+
+    @Test
+    void getBooksTest() {
+        get("BookStore/v1/Books")
+                .then()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void getBooksWithAllLogsTest() {
+        given()
+                .log().all()
+                .when()
+                .get("BookStore/v1/Books")
+                .then()
+                .log().all()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void getBooksWithSomeLogsTest1() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("BookStore/v1/Books")
+                .then()
+                .log().status()
+                .log().body()
+                .body("books", hasSize(greaterThan(0)));
+    }
 }
